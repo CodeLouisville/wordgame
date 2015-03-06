d0d820dac476b16f92342b25237c1f6925b50068
Pranay Aryal <drpranayaryal@gmail.com>
2015-03-06 00:38:39 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -55,16 +55,7 @@ function refreshQuestions(){
             // 30 seconds is up!
             // add functionality here 
             alert("You took too long");
-            $('#countdown').countdown({ 
-        until: +5, 
-        format: '<p>S</p>', 
-        onExpiry: function () {
-            // 30 seconds is up!
-            // add functionality here 
-            alert("You took too long");
-            
-        }   
-    });
+            $(".gbox").hide();
         }   
     });
     
