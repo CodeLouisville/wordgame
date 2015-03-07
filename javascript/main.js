7c66c0144e1c305fec1fa04941d9ea2c95266057
Pranay Aryal <drpranayaryal@gmail.com>
2015-03-06 21:36:28 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -38,6 +38,7 @@ $(document).ready(refreshQuestions);
 //it in another function which can then be called wherever onLoad is called(Pranay)
 function refreshQuestions(){
     console.log("refreshQuestions...");
+    $('#scorebox').hide();
     $('#submit').show();
     $('#countdownbox').show();
     var textGuess = $('#txtguess'); 
